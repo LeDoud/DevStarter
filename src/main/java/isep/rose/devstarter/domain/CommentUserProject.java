f5d90330a31402aba8e5c97a1033934bbf577f14
LeDoud <Edouard@MacBook-Air-de-Edouard.local>
2013-05-02 16:53:02 +0200

    Database & reverse engineer

---
+++ src/main/java/isep/rose/devstarter/domain/CommentUserProject.java
@@ -0,0 +1,13 @@
+package isep.rose.devstarter.domain;
+
+import org.springframework.roo.addon.dbre.RooDbManaged;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(versionField = "", table = "COMMENT_USER_PROJECT")
+@RooDbManaged(automaticallyDelete = true)
+public class CommentUserProject {
+}
